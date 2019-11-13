0467a5161eaa970461cee658658ebc3953ac4528
brenoq <brenoq@gmail.com>
2019-11-12 22:51:47 -0300

    update: Create delete route to AppointmentController and configure nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4fdf3908d3b7ce',
+    pass: '2582ef12bd945d',
+  },
+  default: {
+    from: 'Equipe GoBraber <noreply@gobarber.com>',
+  },
+};
